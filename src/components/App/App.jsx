4f9aeaf806241ba103811a5d2e9ccd5f8e5327f8
Manuel Nas <manuel.nas@outlook.com>
2019-05-01 14:42:16 -0500

    Turned App back into a functional component.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,30 @@
 import { MuiThemeProvider } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import React from 'react';
 import DataContainer from 'components/DataContainer';
 import { THEME } from 'theme/MuiTheme';
 
-class App extends React.PureComponent {
+const App = (props) => {
+	const { match } = props;
+	const { subreddit } = match.params;
 
-	render() {
-		const { match } = this.props;
-		const { subreddit } = match.params;
+	return (
+		<DataContainer>
+			<MuiThemeProvider theme={THEME}>
+				<div className="App">
+					{subreddit}
+				</div>
+			</MuiThemeProvider>
+		</DataContainer>
+	);
+};
 
-		return (
-			<DataContainer>
-				<MuiThemeProvider theme={THEME}>
-					<div className="App">
-						{subreddit}
-					</div>
-				</MuiThemeProvider>
-			</DataContainer>
-		);
-	}
-}
+App.propTypes = {
+	match: PropTypes.shape({
+		params: PropTypes.shape({
+			subreddit: PropTypes.string,
+		}).isRequired,
+	}).isRequired,
+};
 
 export default App;
